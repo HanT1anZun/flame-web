c59aed03cfd404c3e86dc6aa6c0b773707983668
FLAME-Dev <flame@project.local>
2026-05-19 01:27:42 +0800

    fix: change Vite base to relative path to fix black screen on deploy
    /ISO

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/flame-web/" : "/",
+  base: command === "build" ? "./" : "/",
   server: {
     port: 3000,
     proxy: {
